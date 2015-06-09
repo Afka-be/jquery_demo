af7d06dd3057bd48eb8e57a7220cd317bd9899d8
Afka-be <k.fahim24@gmail.com>
2015-06-09 15:21:53 +0200

    Ajouté la fonction Changer au bouton

--- git_exo.js
+++ git_exo.js
@@ -23,5 +23,22 @@ $(document).ready(function() {
             direction='left';
         } else {}
     });
+    
+    $(".change").click(function() {
+        var clicks = $(this).data('clicks');
+        if (clicks) {
+            $("#carre").animate({borderRadius:"0px"});
+        } else {
+            $("#carre").animate({borderRadius:"20px"});
+        }
+        $(this).data("clicks", !clicks);
+
+        //Les propriétés non numériques ne peuvent être animées avec jquery, le Backgroundcolor n'est donc pas animable.
+        if($("#carre").hasClass('carre_style')){
+            $("#carre").removeClass('carre_style');
+        } else {
+            $("#carre").addClass('carre_style');
+        } 
+    });
 
 });
